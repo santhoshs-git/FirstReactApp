d25d4ba9fc8bffde70335d9fcfe9d2351bcb6611
iultokh2 <iultokh2@mail.gitlabdemo.cloud>
2021-07-13 01:01:32 +0530

    made changes in buttons

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Learn ReactJS
 
 Hello all this is a simple react project as part of my React learning journy.
+
--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ const Firstapp=()=> {
         <h1>Hello World This is My First React App</h1>
         <h2>This is second Heading</h2>
         <ul>
-        <li><Button buttontext="Signup" /></li>
-        <li><Button buttontext="Sigin" /></li>
-        <li><Button buttontext="Forgot Password ?"/></li>
+        <li><Button buttontext="HTML" /></li>
+        <li><Button buttontext="JavaScript" /></li>
+        <li><Button buttontext="ReactJS"/></li>
         </ul>
         </div>
     )
